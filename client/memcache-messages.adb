8ebf2a45ddee3fc6fecf068cd20cdb630b5a7041
R. Tyler Croy <tyler@monkeypox.org>
2010-12-05 17:12:40 -0800

    Pass Test_Get_Single_Key

--- client/memcache-messages.adb
+++ client/memcache-messages.adb
@@ -21,11 +21,23 @@ package body Memcache.Messages is
     function Create(Key : in String) return Get is 
         M : Get;
     begin
+        M.Keys.Append(Bounded.To_Bounded_String(Key));
         return M;
     end Create;
 
     function Serialize(M : in Get) return String is
+        function Implode_Keys(M: in Get) return String is
+            Length : Natural := Natural(Key_Vectors.Length(M.Keys));
+        begin
+            if Length = 0 then
+                return "";
+            elsif Length = 1 then
+                return Bounded.To_String(M.Keys.Element(Index => 0));
+            else
+                return "";
+            end if;
+        end Implode_Keys;
     begin
-        return "get\r\n"; 
+        return "get " & Implode_Keys(M) & "\r\n";
     end Serialize;
 end Memcache.Messages;
