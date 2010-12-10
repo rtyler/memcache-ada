5c31b1fefa043410c938804e8db92fc4e8ab276e
R. Tyler Croy <tyler@monkeypox.org>
2010-12-09 17:57:06 -0800

    Remove the Memcache.Messages package, no need for this.
    Will be cleaning up and re-introducing an API in subsequent commits

--- tests/harness/suite.adb
+++ tests/harness/suite.adb
@@ -1,14 +1,13 @@
 with Memcache;
 
-with Test_Messages;
+with Test_Client;
 
 package body Suite is
     use AUnit.Test_Suites;
     function Suite return Access_Test_Suite is
         Result : constant Access_Test_Suite := new Test_Suite;
     begin
-        Result.Add_Test(new Test_Messages.Messages_Test);
-        --Result.Add_Test(new SimpleTest.Test);
+        Result.Add_Test (new Test_Client.Client_Test);
         return Result;
     end Suite;
 end Suite;
