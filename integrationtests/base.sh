1e4fcc1038498cbca487671d36411ebd192b4a16
R. Tyler Croy <tyler@monkeypox.org>
2010-12-12 18:48:14 -0800

    Refactor integration test scripts to be more reusable, clean, fluffy, etc

---
+++ integrationtests/base.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+export PORT=11212
+
+function pyexec() {
+    PYTHONPATH=../../:$PYTHONPATH python -c "${1}";
+}
+
+function killcache() {
+    echo
+    echo ">> Killing cache at pid ${CACHE_PID}"
+    kill ${CACHE_PID}
+}
+
+function runcache() {
+    memcached -p ${PORT} -U 0 -m 1 -vv &
+    export CACHE_PID=$!
+    echo ">> memcached started with pid ${CACHE_PID}";
+    echo ">> Waiting for memcached to come online on port ${PORT}";
+    sleep 1;
+}
+
+function cacheset() {
+    pyexec "import memcache; memcache.Client(('127.0.0.1:${PORT}',)).set('${1}', '${2}')";
+    if [ $? -ne 0 ]; then
+        echo ">> Failed to set key \"${1}\"";
+        exit 15;
+    fi;
+}
+
+function cachekeydne() {
+    # Check to verify that a key does not exist
+    pyexec "import memcache; assert not memcache.Client(('127.0.0.1:${PORT}',)).get('${1}')"
+    if [ $? -ne 0 ]; then
+        echo ">> Failed to verify the key \"${1}\" does not exist";
+        exit 15;
+    fi;
+}
+
--- integrationtests/deleter/deleter.sh
+++ integrationtests/deleter/deleter.sh
@@ -1,39 +1,17 @@
 #!/bin/sh
 
-echo
-echo
+source ../base.sh
 
-PORT=11212
+runcache
 
-function pyexec() {
-    PYTHONPATH=../../:$PYTHONPATH python -c "${1}"
-}
+trap killcache INT QUIT TERM EXIT
 
-
-memcached -p ${PORT} -U 0 -m 1 -vv &
-CACHE_PID=$!
-
-echo ">> memcached started with pid ${CACHE_PID}"
-echo ">> Waiting for memcached to come online on port ${PORT}"
-sleep 1 
-
-pyexec "import memcache; memcache.Client(('127.0.0.1:${PORT}',)).set('deleter', 'foo')"
+cacheset "deleter" "foo"
 
 ./deleter ${PORT}
 
-EXIT_STATUS=$?
-
-if [ "${EXIT_STATUS}" -ne "0" ]; then
-    exit ${EXIT_STATUS};
+if [ $? -ne 0 ]; then
+    exit 15;
 fi;
 
-pyexec "import memcache; assert not memcache.Client(('127.0.0.1:${PORT}',)).get('deleter')"
-EXIT_STATUS=$?
-
-
-echo
-echo ">> Killing pid ${CACHE_PID}"
-kill ${CACHE_PID}
-
-echo ">> Exiting with status ${EXIT_STATUS}"
-exit ${EXIT_STATUS}
+cachekeydne "deleter"
