f7904257ba304d16016b1abd653ceee54bb3fb24
R. Tyler Croy <tyler@monkeypox.org>
2010-12-16 09:46:15 -0800

    Cleanup the integration test runner output

--- integrationtests/runtests.py
+++ integrationtests/runtests.py
@@ -8,12 +8,16 @@ def main():
     for dname in os.listdir('.'):
         if not os.path.isdir(dname):
             continue
+        print
+        print '..................................'
+        print '---  %s'  % dname.upper()
         command = 'sh %(dname)s.sh' % locals()
         exit_code = subprocess.call(command.split(' '), cwd=dname)
 
         if not exit_code == 0:
             print '>> Failed running %s' % dname
             return exit_code
+        print '..................................'
     return 0
 
 if __name__ == '__main__':
